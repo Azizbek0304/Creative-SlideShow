d0c211fbbe54e88a92d2c206e9a529478905f8fa
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-27 23:03:29 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Creative-SlideShow
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-eckchs)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SlideShow from './components/SlideShow';
+import './style.scss';
+
+export default function App() {
+  return (
+    <div>
+      <SlideShow />
+    </div>
+  );
+}
