9466527aa65aef48a0eb1451bb1755bb65599ca9
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 13:17:25 +0500

    2 files modified, 2 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import SlideShow from './components/SlideShow';
+import SlideShow from './components/3dSlide';
+import Navbar from './components/Navbar';
 import './style.scss';
 
 export default function App() {
   return (
     <div>
+      <Navbar />
       <SlideShow />
     </div>
   );
