682b4db4fe40b11589385924a074a8e608529732
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 13:23:35 +0500

    1 file modified, 5 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import SlideShow from './components/3dSlide';
-import Navbar from './components/Navbar';
-import './style.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-export default function App() {
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Testimonial from './pages/Testimonal';
+import Navbar from './components/Navbar';
+import SlideShow from './components/3dSlide';
+
+const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
       <SlideShow />
-    </div>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about" exact>
+            <About />
+          </Route>
+          <Route path="/service" exact>
+            <Services />
+          </Route>
+          <Route path="/testimonial" exact>
+            <Testimonial />
+          </Route>
+          <Route path="/contact" exact>
+            <Contact />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </Router>
   );
-}
+};
+
+export default App;
