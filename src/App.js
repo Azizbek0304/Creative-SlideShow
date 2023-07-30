a23215b416ce456d05e0dda4aaa8d8024f62eae4
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-30 13:44:49 +0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from 'react-router-dom';
+import './style.scss';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Redirect,
+//   Switch,
+// } from 'react-router-dom';
 
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Testimonial from './pages/Testimonial';
-import Navbar from './components/Navbar';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Home from './pages/Home';
+// import Services from './pages/Services';
+// import Testimonial from './pages/Testimonial';
+// import Navbar from './components/Navbar';
+import SlideShow from './components/3dSlide';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/about" exact>
-            <About />
-          </Route>
-          <Route path="/service" exact>
-            <Services />
-          </Route>
-          <Route path="/testimonial" exact>
-            <Testimonial />
-          </Route>
-          <Route path="/contact" exact>
-            <Contact />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <div className="App">
+      <SlideShow />
+    </div>
+    // <Router>
+    //   <Navbar />
+    //   <main>
+    //     <Switch>
+    //       <Route path="/" exact>
+    //         <Home />
+    //       </Route>
+    //       <Route path="/about" exact>
+    //         <About />
+    //       </Route>
+    //       <Route path="/service" exact>
+    //         <Services />
+    //       </Route>
+    //       <Route path="/testimonial" exact>
+    //         <Testimonial />
+    //       </Route>
+    //       <Route path="/contact" exact>
+    //         <Contact />
+    //       </Route>
+    //       <Redirect to="/" />
+    //     </Switch>
+    //   </main>
+    // </Router>
   );
 };
 
